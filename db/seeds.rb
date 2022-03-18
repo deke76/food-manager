bf14954bd9773da4d23efd7a3ecbc9658bfff49d
Darcy <darcy.lauder@shaw.ca>
2022-03-17 21:52:41 -0600

    food_items added, initial migrate & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Only run on development (local) instances not on production, etc.
+unless Rails.env.development?
+  puts "Development seeds only (for now)!"
+  exit 0
+end
+
+puts 'Re-creating food items'
+
+FoodItem.destroy_all
+
+FoodItem.create({
+  name: 'Milk',
+  quantity: '1',
+  quantity_units: 'each',
+  price: '4.29',
+  purchased: '2022-02-14',
+  expires: '2022-02-25'
+})
+
+FoodItem.create({
+  name: 'Cheddar',
+  quantity: '1',
+  quantity_units: 'each',
+  price: '8.49',
+  purchased: '2022-02-14',
+  expires: '2022-04-25'
+})
+
+FoodItem.create({
+  name: 'Banana',
+  quantity: '6',
+  quantity_units: 'each',
+  price: '0.45',
+  purchased: '2022-03-01',
+  expires: '2022-03-08'
+})
+
+FoodItem.create({
+  name: 'Mushroom Soup',
+  quantity: '1',
+  quantity_units: 'each',
+  price: '1.29',
+  purchased: '2022-02-14',
+  expires: '2023-02-14'
+})
