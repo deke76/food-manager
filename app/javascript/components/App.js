77e593e173eec1f7c49f37b6489178d4cbe5ff72
rjp301 <rileypaul96@gmail.com>
2022-03-19 21:53:59 -0600

    Add test button

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import Button from "./Button/Button"
 
 class App extends Component {
   render() {
     return (
-      <h1>Here we go!</h1>
+      <h1>Here we go! <Button label="Test" backgroundColor="green"/></h1>
     )
   }
 }
