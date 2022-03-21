34eb7eaae3c7c16a689fec7f7c0ba5ed122ecf61
rjp301 <rileypaul96@gmail.com>
2022-03-21 10:01:05 -0600

    Change calendar format for mobile

--- client/src/components/calendar/CalendarDay.jsx
+++ client/src/components/calendar/CalendarDay.jsx
@@ -1,39 +1,26 @@
-import CalendarItem from "./CalendarItem";
-import Button from "../buttons/Button";
-
 import moment from "moment";
 import classNames from "classnames";
 import "./CalendarDay.scss";
-import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function CalendarDay(props) {
-  const { day, events, isCalendarStart } = props;
+  const { day, hasEvents } = props;
   const date = moment(day);
-  const [plusShown, setPlusShown] = useState(false);
 
-  // Add month name if first day in calendar or first day of month
-  const addMonth =
-    date.isSame(moment(date).startOf("month"), "day") || isCalendarStart;
-
-  // Assign classes to day object
   const classes = classNames("day", {
     weekend: moment(date).day() === 0 || moment(date).day() === 6,
     today: date.isSame(moment(), "day"),
+    event: hasEvents,
   });
+
   return (
-    <td
-      className={classes}
-      onMouseEnter={() => setPlusShown(true)}
-      onMouseLeave={() => setPlusShown(false)}
-    >
+    <td className={classes}>
       <div className="container">
-        <div className="day-add">{plusShown && <Button icon="plus" />}</div>
-        <div className="day-num">
-          {addMonth && date.format("MMM")} {date.format("D")}
-        </div>
-        <div className="event-container">
-          {events &&
-            events.map((e, index) => <CalendarItem key={index} {...e} />)}
+        <div className="day-num">{date.format("D")}</div>
+        <div className="day-event-indicator">
+          {<FontAwesomeIcon icon={faCircle} />}
         </div>
       </div>
     </td>
