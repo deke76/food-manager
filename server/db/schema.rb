db08f45fe98b00cb6c32cf57be25070c0f63a190
Darcy <darcy.lauder@shaw.ca>
2022-03-22 18:17:45 -0600

    fine tuning for db

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2022_03_22_004322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "food_items", force: :cascade do |t|
-    t.text "name"
-    t.integer "quantity"
-    t.text "quantity_units"
-    t.float "price"
-    t.date "purchased"
-    t.date "expires"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "foods", force: :cascade do |t|
     t.integer "user_id"
     t.text "name"
