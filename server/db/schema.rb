c632feb3e2a7b1bea2a4dedc0476a8801af8cfce
rjp301 <rileypaul96@gmail.com>
2022-03-24 08:44:22 -0600

    Update database.yml to use env

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_005005) do
+ActiveRecord::Schema.define(version: 2022_03_24_004944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2022_03_24_005005) do
 
   create_table "locations", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "temperature"
     t.text "name"
     t.text "street_address"
     t.text "postal"
