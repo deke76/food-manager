d35ae82fa6b13c75aa503c837b42483334a55a9c
rjp301 <rileypaul96@gmail.com>
2022-03-24 09:06:27 -0600

    Updated food controller with recipe and autocomplete routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/foods", to: "foods#all"
   get "/locations", to: "locations#all"
+  get "/foods", to: "foods#all"
+  get "/foods/autocomplete", to: "foods#autocomplete"
+  
+  get "/recipes", to: "foods#recipes_index"
+  get "/recipes/:id", to: "foods#recipes_show"
 
   resources :users do
     get "/foods", to: "foods#user"
