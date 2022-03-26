3b913b9ec27db1dde7529706de764bc2f40bec27
rjp301 <rileypaul96@gmail.com>
2022-03-26 14:58:39 -0600

    rearrange app.js for more control

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,10 @@ import NavBar from "./components/nav/NavBar";
 import FoodAdd from "./components/Food/Add";
 
 import FoodBrowse from "./components/Food/Browse";
-import Landing from "./components/landing";
 import LocationList from "./components/Locations/Browse/locationList";
 
 import FloatingActionButton from "./components/buttons/fab";
+import LocationAdd from "./components/Locations/Add";
 
 // Main application file
 export default function App(props) {
@@ -31,6 +31,7 @@ export default function App(props) {
               <Routes>
                 <Route path="/" element={<FoodBrowse />} />
                 <Route path="/foods/add" element={<FoodAdd />} />
+                <Route path="/locations/add" element={<LocationAdd />} />
               </Routes>
             </div>
             <NavBar />
--- client/src/components/Locations/Browse/locationCardNew.jsx
+++ client/src/components/Locations/Browse/locationCardNew.jsx
@@ -1,12 +1,15 @@
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 export default function LocationCardNew(props) {
   const { onAdd } = props;
   return (
-    <div className="location-card new" onClick={onAdd}>
-      <FontAwesomeIcon icon={faPlusCircle} />
-      <div className="location-card__details">add a new location</div>
-    </div>
+    <Link to="/locations/add">
+      <div className="location-card new" onClick={onAdd}>
+        <FontAwesomeIcon icon={faPlusCircle} />
+        <div className="location-card__details">add a new location</div>
+      </div>
+    </Link>
   );
 }
