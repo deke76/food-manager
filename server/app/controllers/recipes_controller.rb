ad224a320a326c55f1761b0b0605721cc20a638c
Darcy <darcy.lauder@shaw.ca>
2022-03-27 17:34:05 -0600

    remove comments in recipe_controller

--- server/app/controllers/recipes_controller.rb
+++ server/app/controllers/recipes_controller.rb
@@ -1,13 +1,10 @@
 class RecipesController < FoodsController 
   def index
-    puts params[:ingredients]
     uri_string = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + params[:ingredients]
-    puts uri_string
     uri = URI(uri_string)
     spoonacular_query = { :apiKey => ENV['SPOONACULAR_API'], :number => 10, :ingredients => params[:ingredients] }
     uri.query = URI.encode_www_form(spoonacular_query)
     res = Net::HTTP.get_response(uri)
-    puts res.body
     render json: res.body
   end
   
