8701619d666858c360c0b042adcc870be15bcaed
Darcy <darcy.lauder@shaw.ca>
2022-03-28 16:58:00 -0600

    locations integrated with constants

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
-// import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { 
+  BrowserRouter, Routes, Route, 
+  UserProvider, StateProvider,
+  FloatingActionButton, FoodAdd, FoodBrowse,
+  HeaderBar } from "./constants";
 
-import "./styles/app.scss";
+  import LocationList from "./components/Locations/locationList";
+  import RecipeBrowse from "./components/Recipes";
+  
+  import "./styles/app.scss";
 
-import UserProvider from "./providers/UserProvider";
-import StateProvider from "./providers/StateProvider";
-
-import HeaderBar from "./components/header";
-
-import FoodAdd from "./components/Food/Add";
-
-import FoodBrowse from "./components/Food/Browse";
-import LocationList from "./components/Locations/locationList";
-import RecipeBrowse from "./components/Recipes";
-import FloatingActionButton from "./components/buttons/fab";
 
 // Main application file
 export default function App(props) {
--- client/src/components/Locations/locationList.jsx
+++ client/src/components/Locations/locationList.jsx
@@ -1,10 +1,8 @@
-import { useContext, useState } from "react";
-import { userContext } from "../../providers/UserProvider";
-import { stateContext } from "../../providers/StateProvider";
-
-import LocationCard from "./locationCard";
-import LocationCardNew from "./locationCardNew";
-import axios from "axios";
+import { 
+  axios,
+  useContext, useState,
+  userContext, stateContext,
+  LocationCard, LocationCardNew } from "../../constants";
 
 import "./locationList.scss";
 
