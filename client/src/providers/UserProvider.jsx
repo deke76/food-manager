ede76156f2cb25cd15e44c2acf7e36553173961f
Darcy <darcy.lauder@shaw.ca>
2022-03-28 14:43:44 -0600

    formatting

--- client/src/providers/UserProvider.jsx
+++ client/src/providers/UserProvider.jsx
@@ -5,11 +5,6 @@ export const userContext = createContext();
 export default function UserProvider(props) {
   const [user, setUser] = useState(1);
 
-
-  
-  // const incrementUser = () => setUser((prev) => (prev < 20 ? prev + 1 : prev));
-  // const decrementUser = () => setUser((prev) => (prev > 1 ? prev - 1 : prev));
-
   const providerData = { user, setUser };
 
   return (
