a9586e837c8c2a6ece6df9114ec459a886fccc2a
rjp301 <rileypaul96@gmail.com>
2022-03-27 18:32:28 -0600

    change food add save buttons

--- client/src/components/Food/Add/index.jsx
+++ client/src/components/Food/Add/index.jsx
@@ -1,6 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarcode } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+
+import { stateContext } from "./../../../providers/StateProvider";
+import { userContext } from "../../../providers/UserProvider";
 
 import Counter from "../../buttons/counter";
 import SelectOneDropdown from "../../buttons/selectOne";
@@ -16,30 +19,18 @@ export default function FoodAdd(props) {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [selectedSuggestion, setSelectedSuggestion] = useState(null);
 
-  // New Food Values
-  const [foodName, setFoodName] = useState("New Food Item");
-  const [foodDateExpiry, setFoodDateExpiry] = useState(
-    moment().add(10, "days").format("YYYY-MM-DD")
-  );
-  const [foodDatePurchase, setFoodDatePurchase] = useState(
-    moment().format("YYYY-MM-DD")
-  );
+  const { state } = useContext(stateContext);
+  const { user } = useContext(userContext);
 
   const defaultFood = {
-    location_id: 1,
-    name: "",
-    quantity: 4,
+    name: "Add Food Item...",
+    quantity: 1,
     quantity_units: "ea",
-    price_cents: 1454,
-    date_purchased: "2022-03-23",
-    date_expires: "2022-04-02",
+    date_purchased: moment().format("YYYY-MM-DD"),
+    date_expires: moment().add(10, "days").format("YYYY-MM-DD"),
   };
 
-  const [newFood, setNewFood] = useState();
-
-  const [foodQtyNum, setFoodQtyNum] = useState(1);
-  const [foodQtyUnit, setFoodQtyUnit] = useState(0);
-  const [showFoodQty, setShowFoodQty] = useState(false);
+  const [newFood, setNewFood] = useState(defaultFood);
   const foodUnitOptions = ["ea", "oz", "gal", "L", "kg", "lb"];
 
   // const save = () => {
@@ -48,31 +39,43 @@ export default function FoodAdd(props) {
 
   // Fetch suggestions from server
   useEffect(() => {
-    const url = "http://localhost:3000/foods/autocomplete";
-    const params = searchValue.length > 0 ? { query: searchValue } : null;
+    if (searchValue.length > 0) {
+      const url = "http://localhost:3000/foods/autocomplete";
+      const params = { query: searchValue };
 
-    axios
-      .get(url, { params })
-      .then((response) => setSuggestions(response.data))
-      .catch((e) => {
-        console.error(e.stack);
-        setSuggestions([]);
-      });
+      axios
+        .get(url, { params })
+        .then((response) => setSuggestions(response.data))
+        .catch((e) => {
+          console.error(e.stack);
+          setSuggestions([]);
+        });
+    } else {
+      setSuggestions([]);
+    }
   }, [searchValue]);
 
+  // set newFood state whenever qty counter changes
+  const [showFoodQty, setShowFoodQty] = useState(false)
+  const [foodQtyNum, setFoodQtyNum] = useState(1);
+  const [foodQtyUnit, setFoodQtyUnit] = useState(0);
   useEffect(() => {
-    suggestions &&
-      selectedSuggestion !== null &&
-      setFoodName(suggestions[selectedSuggestion].name.toUpperCase());
-  }, [selectedSuggestion, suggestions]);
+    setNewFood((prev) => ({
+      ...prev,
+      quantity: foodQtyNum,
+      quantity_units: foodQtyUnit,
+    }));
+  },[foodQtyNum, foodQtyUnit]);
 
-  useEffect(() => {
-    searchValue === "" && setSuggestions([]);
-  }, [searchValue]);
+  // useEffect(() => {
+  //   suggestions &&
+  //     selectedSuggestion !== null &&
+  //     setFoodName(suggestions[selectedSuggestion].name.toUpperCase());
+  // }, [selectedSuggestion, suggestions]);
 
-  return (
+  return defaultFood ? (
     <div className="food-add">
-      <h1>{foodName}</h1>
+      <h1>{newFood.name}</h1>
       <div className="food-add__input">
         <input
           type="text"
@@ -81,41 +84,54 @@ export default function FoodAdd(props) {
             setSelectedSuggestion(null);
             setSearchValue(e.target.value);
           }}
-          onClick={() => setShowSuggestions(true)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
           placeholder="Search for food..."
         />
         <div className="barcode-btn">
           <FontAwesomeIcon icon={faBarcode} />
         </div>
       </div>
-      {showSuggestions && (
-        <SelectOneDropdown
-          selected={selectedSuggestion}
-          setSelected={setSelectedSuggestion}
-          choices={suggestions.map((suggestion) => suggestion.name)}
-          onClickCallback={() => setShowSuggestions(false)}
-        />
-      )}
+      {showSuggestions &&
+        (suggestions.length > 0 ? (
+          <SelectOneDropdown
+            selected={selectedSuggestion}
+            setSelected={setSelectedSuggestion}
+            choices={suggestions.map((suggestion) => suggestion.name)}
+            onClickCallback={() => setShowSuggestions(false)}
+          />
+        ) : (
+          <div>No Suggestions</div>
+        ))}
       <div className="food-add__details">
         <div className="group">
           <label>Date Purchased</label>
           <input
             type="date"
-            value={foodDatePurchase}
-            onChange={(e) => setFoodDatePurchase(e.target.value)}
+            value={newFood.date_purchased}
+            onChange={(event) =>
+              setNewFood((prev) => ({
+                ...prev,
+                date_purchased: event.target.value,
+              }))
+            }
           />
         </div>
         <div className="group">
           <label>Date Expires</label>
           <input
             type="date"
-            value={foodDateExpiry}
-            onChange={(e) => setFoodDateExpiry(e.target.value)}
+            value={newFood.date_expires}
+            onChange={(event) =>
+              setNewFood((prev) => ({
+                ...prev,
+                date_expires: event.target.value,
+              }))
+            }
           />
         </div>
         <div className="group">
           <label>Quantity</label>
-
           <div className="group">
             <Counter
               value={foodQtyNum}
@@ -138,9 +154,10 @@ export default function FoodAdd(props) {
         )}
       </div>
       <div className="group">
-        <button>Save and Exit</button>
-        <button>Save and Add Another</button>
+        <button>SAVE</button>
       </div>
     </div>
+  ) : (
+    <div>Error: Could not read data</div>
   );
 }
