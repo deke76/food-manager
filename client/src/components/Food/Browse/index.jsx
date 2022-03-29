a6fb0e06d17d028a8270e74663097bd8b17c5004
rjp301 <rileypaul96@gmail.com>
2022-03-29 13:00:39 -0600

    food list displays

--- client/src/components/Food/Browse/index.jsx
+++ client/src/components/Food/Browse/index.jsx
@@ -9,10 +9,12 @@ export default function FoodBrowse(props) {
     ? state.locations.filter((loc) => loc.id === state.currentLocation)[0]
     : null;
 
+  const foods = state ? state.foods.filter(food => food.location_id === selectedLocation.id) : []
+  
   return selectedLocation ? (
     <div className='food-container'>
       {/* <div>{<Calendar />}</div> */}
-      <div>{<FoodList foods={selectedLocation.foods} />}</div>
+      <div>{<FoodList foods={foods} />}</div>
     </div>
   ) : (
     <div>Could not fetch data</div>
