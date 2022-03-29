a23818100e05aec597a5de23b5106addc496267b
Darcy <darcy.lauder@shaw.ca>
2022-03-28 21:02:14 -0600

    server adjustments

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -4,16 +4,12 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-  def show
-    @users = User.find(params[:id])
-    render json: @users
+  def email
+    @user = params[:id]
+    render json params
+    # Tell the UserMailer to send a welcome email after save
+    # UserMailer.with(user: @user).welcome_email.deliver_now
+    # format.html { redirect_to(@user, notice: 'User was successfully created.') }
+    # format.json { render json: @user, status: :created, location: @user }
   end
-
-
-  # def show
-  #   locations = Location.where(user_id: params[:id])
-  #   foods = Food.where(user_id: params[:id])
-  #   @user = { 'locations' => locations, 'shelves' => shelves, 'foods' => foods }
-  #   render json: @user
-  # end
 end
