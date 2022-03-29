993b4953e3c0e5714c50a18f0ac524861c553f62
Darcy <darcy.lauder@shaw.ca>
2022-03-28 20:21:02 -0600

    tweak userprovider import to defaul

--- client/src/providers/UserProvider.jsx
+++ client/src/providers/UserProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const userContext = createContext();
 
-export function UserProvider(props) {
+export default function UserProvider(props) {
   const [user, setUser] = useState(1);
 
 
