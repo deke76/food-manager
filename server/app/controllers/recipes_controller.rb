fb5987a119d330bad55dc804e55a45433c898cd2
rjp301 <rileypaul96@gmail.com>
2022-04-01 08:43:27 -0600

    Add gifs of functionality

--- server/app/controllers/recipes_controller.rb
+++ server/app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < FoodsController
     uri = URI(uri_string)
     spoonacular_query = { :apiKey => ENV['SPOONACULAR_API'], :number => 10, :ingredients => params[:ingredients] }
     uri.query = URI.encode_www_form(spoonacular_query)
-    # res = Net::HTTP.get_response(uri)
+    res = Net::HTTP.get_response(uri)
     render json: res.body
   end
 
@@ -13,7 +13,7 @@ class RecipesController < FoodsController
     uri = URI("https://api.spoonacular.com/recipes/#{id}/information")
     spoonacular_query = { :apiKey => ENV['SPOONACULAR_API'] }
     uri.query = URI.encode_www_form(spoonacular_query)
-    # res = Net::HTTP.get_response(uri)
+    res = Net::HTTP.get_response(uri)
     render json: res.body
   end
 
